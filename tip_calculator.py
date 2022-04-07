eca2b860119ff16a332df1018cd2ab90bec8b4f4
Chase Childers <chasechilders12@gmail.com>
2022-04-07 18:30:32 -0400

    tip calculator complete

---
+++ tip_calculator.py
@@ -0,0 +1,9 @@
+bill_total = float(input("What is the total bill: "))
+tip_percentage = int(input("What percentage would you like to tip? "))
+
+def percentage_plus(bill_total, tip_percentage):
+  tip_amount = bill_total * (tip_percentage / 100)
+  return bill_total + tip_amount
+
+final_amount = percentage_plus(bill_total, tip_percentage)
+print (f"The final amount is {final_amount}")
