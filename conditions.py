33cb421f853cdfe13e7995f6a13d93bec39cfe47
Chase Childers <chasechilders12@gmail.com>
2022-04-07 19:52:15 -0400

    picnic game complete

--- conditions.py
+++ conditions.py
@@ -103,16 +103,27 @@
 
 # Letter Grade
 
-letterGrade = int(input('Enter a score 1-100 '))
-if letterGrade < 60:
-  print("F")
-elif letterGrade < 70 and letterGrade >= 60:
-  print("D")
-elif letterGrade <80 and letterGrade >= 70:
-  print("C")
-elif letterGrade <90 and letterGrade >= 80:
-  print("B")
-elif letterGrade <= 100 and letterGrade >= 90:
-  print("A")
-else:
-  print("Enter a valid score")
+# letterGrade = int(input('Enter a score 1-100 '))
+# if letterGrade < 60:
+#   print("F")
+# elif letterGrade < 70 and letterGrade >= 60:
+#   print("D")
+# elif letterGrade <80 and letterGrade >= 70:
+#   print("C")
+# elif letterGrade <90 and letterGrade >= 80:
+#   print("B")
+# elif letterGrade <= 100 and letterGrade >= 90:
+#   print("A")
+# else:
+#   print("Enter a valid score")
+
+def same_letter(input1, input2):
+  if (input1[0]) == (input2[0]):
+    print("You can come to the party")
+  else:
+    print("Try again")
+
+input1 = input("What is your name? ")
+input2 = input("What food are you bringing to the picnic? ")
+
+same_letter(input1, input2)
