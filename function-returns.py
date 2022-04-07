873572accfd2336a552ce2f2dfde118c010c5183
Chase Childers <chasechilders12@gmail.com>
2022-04-07 14:18:39 -0400

    added new functions

---
+++ function-returns.py
@@ -0,0 +1,47 @@
+import math
+
+# def add (a, b):
+#   return a + b
+
+# def mult(a,b):
+#   return a * b
+
+
+# try:
+#   guest_1_pancakes = int (input('How many pancakes? '))
+#   guest_2_pancakes = int (input('and for your guest? '))
+# except:
+#   print("that's not a number!")
+#   exit()
+
+# total = mult(add(guest_1_pancakes, guest_2_pancakes), 1.12)
+# total = (mult(add(10,5), 1.12))
+# total = mult(15,1.12)
+# total = 16.8
+
+# print(total)
+
+# def pancake_calculator (guests, pancakes):
+#   print (math.ceil(guests * pancakes + (.12 * guests * pancakes)))
+
+# pancake_calculator(10, 3)
+
+def mult(a, b):
+  return a * b
+
+try:
+  pancakes = int(input('How many pancakes? '))
+  guests = int(input('and how many guests? '))
+except:
+  print("that's not a number!")
+  exit()
+
+total = math.ceil(mult(mult(pancakes, guests), 1.12))
+
+print(f"You will need {total} pancakes for breakfast :)")
+
+# farenheit to celsius
+
+def farenheitToCelsius (a):
+  try:
+    
---
+++ numbers.py
@@ -0,0 +1,86 @@
+import math
+
+
+name = "Nelson" 
+
+# integer
+weight = 12
+# float
+age = 4.1
+
+# Operators
+
+# print(3 + 3) = 6
+# print(3 + 3.0) = 6.0
+# print(3 / 2) = 1.5
+# print(3 / 3) = 1.0
+# print(2 * 10) = 20
+# print(10 - 5) = 5
+# print(10 % 3) = 1 (gives us the remainder)
+# print(2 ** 5) = 32 (2 to the power of 5)
+# print(10 // 3) = 3 (how many times can it be divided evenly)
+
+# Assignment Operators
+# num = 10
+# #addition assignment operator
+# num += 1
+# print(num)
+
+# num -= 4
+# num *= 2
+# num /= 3
+# #print(num) = 4.6666666666666
+
+# # PEMDAS
+# print(((10 +1 - 4) * 2) / 3)
+
+#
+# print("Nelson is " + age) # TypeError
+# print("Nelson is " + str(age)) # converts age into a string
+
+# age = "10"
+# print(age * 2) # prints the age out twice
+
+# age = "10"
+# new_age = int(age) * 5 # changes age to an integer
+# print(new_age) # this equals 50
+
+# age = "2.5"
+# new_age = float(age) * 5 # changes age to a float
+# print(new_age) = # 12.5
+
+# Calc Methods
+
+def add (a, b):
+  print(a + b)
+
+def subtract (a, b):
+  print(a - b)
+
+def multiply (a, b):
+  print(a * b)
+
+def divide (a, b):
+  print(a / b)
+
+def all_functions (a, b):
+  add(a, b)
+  subtract(a, b)
+  multiply (a, b)
+  divide (a, b)
+
+all_functions (56, 2)
+
+# Pancake Calculator
+
+def pancake_calculator (guests, pancakes):
+  print (math.ceil(guests * pancakes + (.12 * guests * pancakes)))
+
+pancake_calculator(10, 3)
+
+# Temperature Conversion
+
+def farenheitToCelsius (temperature):
+  print(temperature - 32)
+  print(temperature *= 5)
+  print(temperature /= 9)
