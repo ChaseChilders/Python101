628d9797189d8a292943e7328e6c0b4d0ee190b7
Chase Childers <chasechilders12@gmail.com>
2022-04-08 10:50:23 -0400

    finished tip calculator

--- madlib.py
+++ madlib.py
@@ -1,8 +1,8 @@
+def madlib (a, b, c):
+  print(f"Once upon a time there was a knight named {name}. He was a very brave knight. Once, he went to the {noun} and found very large amounts of food. He decided to {verb} and storm out of the {noun} and get on his horse.")
+
 noun = input("Please enter a noun (place): ")
 verb = input("Please enter a present tense verb: ")
 name = input("Put any name here: ")
 
-def madlib ():
-  print(f"Once upon a time there was a knight named {name}. He was a very brave knight. Once, he went to the {noun} and found very large amounts of food. He decided to {verb} and storm out of the {noun} and get on his horse.")
-
-madlib()
+madlib(noun, verb, name)
--- tip_calculator.py
+++ tip_calculator.py
@@ -1,12 +1,18 @@
-bill_total = float(input("What is the total bill: "))
-tip_percentage = int(input("What percentage would you like to tip? "))
+# bill_total = float(input("What is the total bill: "))
+# tip_percentage = int(input("What percentage would you like to tip? "))
 
 def percentage_plus(bill_total, tip_percentage):
   tip_amount = bill_total * (tip_percentage / 100)
-  return bill_total + tip_amount
+  final_total = bill_total + tip_amount
+  return final_total
 
-final_amount = percentage_plus(bill_total, tip_percentage)
-print (f"The final amount is {final_amount}")
+def tip_calculator():
+  bill_total = float(input("What is the total bill: "))
+  tip_percentage = int(input("What percentage would you like to tip? "))
+  final_total = percentage_plus(bill_total, tip_percentage)
+  print(f"Your total is {final_total}")
+
+tip_calculator()
 
 # def percentage_plus(bill_total, tip_percentage):
 #   tip_amount = bill_total * (tip_percentage / 100)
