1bd58e4573f91a7464efe1da0deeca7e447b89fb
Chase Childers <chasechilders12@gmail.com>
2022-04-09 19:43:25 -0400

    created a program that prints out how many years the user has until they are 100

--- practice.py
+++ practice.py
@@ -1,36 +1,43 @@
-def say_hello():
-  name = input('Hello! what is your name? ')
-  print(f'Hello, {name}!')
+# def say_hello():
+#   name = input('Hello! what is your name? ')
+#   print(f'Hello, {name}!')
 
-say_hello()
+# say_hello()
 
-def how_old():
-  age = input('How old are you? ')
-  print(f'You are {age} years old!')
-  print("Congratulations on making it that far")
+# def how_old():
+#   age = input('How old are you? ')
+#   print(f'You are {age} years old!')
+#   print("Congratulations on making it that far")
 
-how_old()
+# how_old()
 
-def user_job():
-  occupation = input('What is your occupation? ')
-  print(f"{occupation}, that is a very interesting job")
+# def user_job():
+#   occupation = input('What is your occupation? ')
+#   print(f"{occupation}, that is a very interesting job")
 
-user_job()
+# user_job()
 
-Question = input("Do you want to play a Trivia game? ")
-if Question == ("Yes"):
-  print("Very well, let's begin")
-elif Question == ("No"):
-  print("No worries, maybe some other time :)")
+# Question = input("Do you want to play a Trivia game? ")
+# if Question == ("Yes"):
+#   print("Very well, let's begin")
+# elif Question == ("No"):
+#   print("No worries, maybe some other time :)")
 
-Question2 = input("Who is known as the GOAT of soccer? ")
-if Question2 == ("Lionel Messi"):
-  print("Correct!")
-elif Question2 != ("Lionel Messi"):
-  print("Sorry, that is not correct")
+# Question2 = input("Who is known as the GOAT of soccer? ")
+# if Question2 == ("Lionel Messi"):
+#   print("Correct!")
+# elif Question2 != ("Lionel Messi"):
+#   print("Sorry, that is not correct")
 
-Question3 = input("What is 42 + 2 - 6? ")
-if Question3 == ("38"):
-  print("You're a math wiz!!")
-elif Question3 != ("38"):
-  print("Oof, not quite")
+# Question3 = input("What is 42 + 2 - 6? ")
+# if Question3 == ("38"):
+#   print("You're a math wiz!!")
+# elif Question3 != ("38"):
+#   print("Oof, not quite")
+
+user_name = input("What is your name?: ")
+user_age = int(input("How old are you? "))
+
+till_they_100 = 100 - user_age
+
+print(f"Hi {user_name}, you have {till_they_100} years until you're 100 :) ")
