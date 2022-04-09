8efbb076218fbb35992c4e8cc84ad9404439b17b
Chase Childers <chasechilders12@gmail.com>
2022-04-09 15:24:43 -0400

    finished dice game

--- dice_roller.py
+++ dice_roller.py
@@ -1,3 +1,5 @@
+# single number dice roller
+
 import random
 
 print("Lets roll a dice! ")
@@ -8,3 +10,16 @@ def function (dice_sides):
   print(roll)
 
 function(dice_sides)
+
+# dice roller between two user inputs
+
+number_1 = int(input("Pick a number: "))
+number_2 = int(input("Pick another number higher than the last one: "))
+
+def dice_roller(number_1, number_2):
+  if number_1 < number_2:
+    random.randint(number_1, number_2)
+    dice_number = random.randint(number_1, number_2)
+    print(dice_number)
+
+dice_roller(number_1, number_2)
