04914243a2ec146bcfca60270764150658d3b8a7
Chase Childers <chasechilders12@gmail.com>
2022-04-09 19:34:48 -0400

    updated calculator to run infinitely

--- coin_flip.py
+++ coin_flip.py
@@ -1,12 +1,26 @@
+# import random
+
+# def coin_flip ():
+#   click_start = int(input("Type 3 to flip the coin: "))
+#   flip = random.randint(0,1)
+#   if click_start == 3:
+#     if flip == 0:
+#       print("Heads")
+#     if flip == 1:
+#       print("Tails")
+#   elif click_start != 3:
+#     print("Type 3 moron ")
+
 import random
 
-def coin_flip ():
-  click_start = int(input("Type 3 to flip the coin: "))
+def coin_flip():
+  user_type = input("Type x to flip a coin: ")
   flip = random.randint(0,1)
-  if click_start == 3:
-    if flip == 0:
-      print("Heads")
+  if user_type == "x":
     if flip == 1:
+      print("Heads")
+    if flip == 0:
       print("Tails")
-  elif click_start != 3:
-    print("Type 3 moron ")
+
+while True:
+  coin_flip()
